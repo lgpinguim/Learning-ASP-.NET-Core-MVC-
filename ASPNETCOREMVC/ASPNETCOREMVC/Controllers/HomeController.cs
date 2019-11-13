4d3689968f008278a74c2954b73a29795c105e22
lgpinguim <lg_pinguim@hotmail.com>
2019-11-12 21:23:25 -0300

    First Commit
    A simple application to showcase the way that the MVC pattern works.

---
+++ ASPNETCOREMVC/ASPNETCOREMVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCOREMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCOREMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpPost]
+        [HttpGet]
+        public IActionResult Index(HomeModel form)
+        {
+            LoadDataList();
+            return View();
+        }
+
+
+        public void LoadDataList()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            ViewBag.list = objHomeModel.GetAll(); // send information between the view and the controller
+        }
+
+    }
+}
---
+++ ASPNETCOREMVC/ASPNETCOREMVC/Models/HomeModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCOREMVC.Models
+{
+    public class HomeModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<HomeModel> GetAll()
+        {
+            HomeModel item;
+            List<HomeModel> list = new List<HomeModel>();
+
+            item = new HomeModel();
+            item.Id = 1;
+            item.Name = "Luis";
+            list.Add(item);
+
+            item = new HomeModel();
+            item.Id = 2;
+            item.Name = "Ferreira";
+            list.Add(item);
+
+            return list;
+
+        }
+    }
+}
